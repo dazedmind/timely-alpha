efaca1a5a588802d08c8fcce17ec1e04e23dc1b4
dazedmind <dazedmind1@protonmail.com>
2022-02-10 10:20:08 +0800

    first commit

---
+++ script/animation.js
@@ -0,0 +1,7 @@
+function openMenu() {
+    document.getElementById('menu-list').style.cssText = 'display:block;';
+}
+
+function closeMenu() {
+    document.getElementById('menu-list').style.cssText = 'display:none';
+}
